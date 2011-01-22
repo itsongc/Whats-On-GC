29c21606dee44adf78af958c4666e36fdb437659
Zarne Dravitzki <itsongc@gmail.com>
2011-01-22 11:06:10 +1000

    First Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whatson::Application.config.secret_token = 'ac56f3d6e3337ee8505314ffb685f8bd36e6ca976eb074ecf5be3cc203d4b3f236677ad5a7b95bde35bee9cbbee68e9c1cc55d07da8658d1db2beb38b7d3de01'
